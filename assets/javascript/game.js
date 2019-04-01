130a29b9dac162410e45f0eeb3c974d35a12cd16
Julio Bell <juliobell2014@gmail.com>
2019-03-31 22:58:38 -0400

    Added sound to button clicks and messages

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,8 +16,23 @@
     var session_wins;
     var session_losses;
 
+    var myAudio;
+    var myAudio1;
     
-    
+  //   function sound(src) {
+  //     this.sound = document.createElement("audio");
+  //     this.sound.src = src;
+  //     this.sound.setAttribute("preload", "auto");
+  //     this.sound.setAttribute("controls", "none");
+  //     this.sound.style.display = "none";
+  //     document.body.appendChild(this.sound);
+  //     this.play = function(){
+  //         this.sound.play();
+  //     }
+  //     this.stop = function(){
+  //         this.sound.pause();
+  //     }    
+  // }
     function generateRandomNumber(min,max) {
       //Math.floor(Math.random() * (maxNumber - minNumber) + minNumber) returns a random integer number between minNumber and maxNumber both inclusive.
       //The random number shown at the start of the game should be between 19 - 120
@@ -66,7 +81,7 @@
       randomCrystal2 = 0;
       randomCrystal3 = 0;
       randomCrystal4 = 0; 
-
+      
        targetScore = generateRandomNumber(19,120);
        $("#random").html('<h2 class="h2 p-2">' + targetScore + '</h2>');
        $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
@@ -78,17 +93,23 @@
 
     initializeGame();
 
+        myAudio = new Audio('assets/sound/win-sound-2.mp3');
+        myAudio1 = new Audio('assets/sound/button-sound.mp3');
+
         //When the player clicks on a crystal, it will add a specific amount of points to the player's total score
         $('#crystal1').on("click", function(){
+          myAudio1.play();
           //Each crystal should have a random hidden value between 1 - 12 
           randomCrystal1 = generateRandomNumber(1,12);
           addToScore(randomCrystal1);
+          // myAudio.stop();
           // totalScore += randomCrystal1;
           // $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
           // crystalNumbers.push(randomCrystal1);
         });
 
         $('#crystal2').on("click", function(){
+          myAudio1.play();
           //Each crystal should have a random hidden value between 1 - 12 
           randomCrystal2 = generateRandomNumber(1,12);
           addToScore(randomCrystal2);
@@ -98,32 +119,39 @@
         });
 
         $('#crystal3').on("click", function(){
+          myAudio1.play();
           //Each crystal should have a random hidden value between 1 - 12 
           randomCrystal3 = generateRandomNumber(1,12);
           addToScore(randomCrystal3);
+          
           // totalScore += randomCrystal3;
           // $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
           // crystalNumbers.push(randomCrystal3);
         });
 
         $('#crystal4').on("click", function(){
+          myAudio1.play();
           //Each crystal should have a random hidden value between 1 - 12 
           randomCrystal4 = generateRandomNumber(1,12);
           addToScore(randomCrystal4);
+          // myAudio.stop();
           // totalScore += randomCrystal4;
           // $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
           // crystalNumbers.push(randomCrystal4);
         });
     
         $(".crystal").on("click", function() {
+          
           if (totalScore === targetScore) {
+            myAudio.play();
             wins++;
             session_wins = wins;
             $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
             
             $('#wins').text('Wins:  ' + session_wins);
             setTimeout(function() {
-              $('#message').html('<h3 class="h3 p-1">' + ' CONGRATS, YOU WON!' + '</h3>').css("background-color", "#7fffd4");
+              $('#message').html('<h3 class="h3 p-1">' + ' CONGRATS, YOU WON!' + '</h3>').css({"background-color": "red", "color": "black"});
+              
               $('#playAgain').html('<button type="button" class="btn btn-warning btn-sm">' + ' PLAY AGAIN ' + '</button>');
               $('#resetGame').html('<button type="button" class="btn btn-danger btn-sm">' + ' RESET GAME ' + '</button>');
             }, 100);
@@ -137,12 +165,14 @@
           }
           else if (totalScore > targetScore) {
             losses++;
+            myAudio.play();
             session_losses = losses;
             $('#score').html('<h3 class="h3 p-1">' + totalScore + '</h3>');
             
             $('#losses').text('Losses:  ' + session_losses);
             setTimeout(function() {
               $('#message').html('<h3 class="h3 p-1">' + ' SORRY, YOU LOSE!' + '</h3>').css({"background-color": "red", "color": "white"});
+              // myAudio1.play();
               $('#playAgain').html('<button type="button" class="btn btn-warning btn-sm">' + ' PLAY AGAIN ' + '</button>');
               $('#resetGame').html('<button type="button" class="btn btn-danger btn-sm">' + ' RESET GAME ' + '</button>');
             }, 100);
